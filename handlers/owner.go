08c50d60005d9698c44cc643ba6327f07ce942da
neoCipher (Kaleab Alemayehu) <cipherlofi04@gmail.com>
2025-03-07 07:04:20 -0500

    refactor: making handler into its own package

--- handlers/owner.go
+++ handlers/owner.go
@@ -1,1 +1,32 @@
 package handlers
+
+import (
+	"net/http"
+
+	"encoding/json"
+	connection "github.com/kaleabAlemayehu/drivee-server/connection"
+	model "github.com/kaleabAlemayehu/drivee-server/model"
+	"log"
+	"os"
+)
+
+func HandleGetAllOwner(w http.ResponseWriter, r *http.Request) {
+	ctx, conn := connection.DBConnect(os.Getenv("GOOSE_DBSTRING"))
+	// TODO:  fetch owner form database
+	query := model.New(conn)
+	owners, err := query.ListOwner(ctx)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "unable to fetch owner list", http.StatusInternalServerError)
+		return
+	}
+
+	// TODO:  sender owner  to the client
+	err = json.NewEncoder(w).Encode(&owners)
+	if err != nil {
+		log.Println("unable to send owner list")
+		http.Error(w, "unable to send owner list", http.StatusInternalServerError)
+		return
+	}
+	return
+}
--- main.go
+++ main.go
@@ -1,18 +1,14 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
 
-	// "github.com/google/uuid"
-	// "github.com/google/uuid"
 	"github.com/joho/godotenv"
-	connection "github.com/kaleabAlemayehu/drivee-server/connection"
-	"github.com/kaleabAlemayehu/drivee-server/model"
+	"github.com/kaleabAlemayehu/drivee-server/handlers"
 )
 
 func main() {
@@ -25,27 +21,8 @@ func main() {
 		port = 9999
 		log.Printf("port is not specified\n running on default port :%d \n", port)
 	}
-	ctx, conn := connection.DBConnect(os.Getenv("GOOSE_DBSTRING"))
 	mux := http.NewServeMux()
-	mux.HandleFunc(fmt.Sprintf("%s /api/owner", http.MethodGet), func(w http.ResponseWriter, r *http.Request) {
-		// TODO:  fetch owner form database
-		query := model.New(conn)
-		owners, err := query.ListOwner(ctx)
-		if err != nil {
-			log.Println(err.Error())
-			http.Error(w, "unable to fetch owner list", http.StatusInternalServerError)
-			return
-		}
-
-		// TODO:  sender owner  to the client
-		err = json.NewEncoder(w).Encode(&owners)
-		if err != nil {
-			log.Println("unable to send owner list")
-			http.Error(w, "unable to send owner list", http.StatusInternalServerError)
-			return
-		}
-		return
-	})
+	mux.HandleFunc(fmt.Sprintf("%s /api/owner", http.MethodGet), handlers.HandleGetAllOwner)
 
 	http.ListenAndServe(fmt.Sprintf(":%d", port), mux)
 }
